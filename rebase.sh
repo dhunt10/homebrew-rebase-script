a00db207a0d6e5e7a05ac619b5414369ae278f10
Darin Hunt <darin@mercari.com>
2019-06-14 09:19:38 -0400

    had to change location of script

---
+++ rebase.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+#must be done in bash shell, exit zsh/other for this to work
+
+git checkout <branch name>
+FILES=$(git diff master.. --name-only)
+git checkout master
+git pull
+git checkout -b NEW_BRANCH
+for file in $FILES; do git checkout <branch name> $file; done
+git commit -m 'message'
+git push -f -u origin NEW_CAT_BRANCH:<branch name>
+git branch -D <branch name>
+git branch -M <branch name>
