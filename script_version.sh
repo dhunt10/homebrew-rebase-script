4dc6e0efcadfd8c8a6689697154a86cb11f8adf0
Darin Hunt <40468660+dhunt10@users.noreply.github.com>
2020-02-12 10:26:54 -0500

    Update script_version.sh

--- script_version.sh
+++ script_version.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
-bash
+#bash
 
 cd $(git rev-parse --show-toplevel) # need to be in root directory
-#must be done in bash shell, exit zsh/other for this to work
 
-read -p 'Name of current branch: ' $(branch_name)
+read -p 'Name of current branch: ' branch_name
+
 git checkout $(branch_name)
+
 FILES=$(git diff master.. --name-only)
-git checkout master
+
+git checkout master                                                                                                                                                                                         
+
 git pull
+
 git checkout -b NEW_BRANCH
+
 for file in $FILES; do git checkout $(branch_name) $file; done
+
 git commit -m 'message'
-git push -f -u origin NEW_BRANCH:$(branch_name)
-git branch -D $(branch_name)
-git branch -M $(branch_name)
+
+git push -f -u origin NEW_BRANCH:$branch_name
+
+git branch -D $branch_name
+
+git branch -M $branch_name
