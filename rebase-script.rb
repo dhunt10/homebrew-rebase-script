a1f1b26d5fddd5ab3b12c7a37679403951022517
Darin Hunt <40468660+dhunt10@users.noreply.github.com>
2020-02-27 15:38:06 -0500

    Update rebase-script.rb

--- rebase-script.rb
+++ rebase-script.rb
@@ -3,7 +3,7 @@ class RebaseScript < Formula
   homepage "https://github.com/dhunt10/homebrew-rebase-script"
   url "https://github.com/dhunt10/homebrew-rebase-script/blob/development/rebase-script1.sh.gz"
   version "1.0.0"
-  sha256 "1c3d3f3a7bf2140cd59df700cdaa9df963e3cb39f748a727949f5e4d4631b73a"
+  sha256 "c93e68284d76241c91839b4b7d2cc75394d846679262f5ae24fea64ef092f8b0"
 
   def install
     bin.install "bin/rebase-script"
