ab01fed70cb953185d8006990a8c484d2e306502
Darin Hunt <dhunt@bo-mbp-c02p739fg3qd.carboniteinc.com>
2020-02-27 15:33:29 -0500

    added stuff

--- rebase-script.rb
+++ rebase-script.rb
@@ -1,7 +1,7 @@
 class Notes < Formula
   desc "..."
   homepage "https://github.com/dhunt10/rebase-script"
-  url "https://github.com/dhunt10/rebase-script/blob/development/rebase-script.sh.gz"
+  url "https://github.com/dhunt10/rebase-script/blob/development/rebase-script1.sh.gz"
   version "1.0.0"
   sha256 "cb245f4f0c656816f33e95fb09d7846defafa153734361f6af0ed832a9857380"
 
